f9114d7b416c960a70a2098a526e6611a49b43b2
daniilkuz <d.d.kuzovlev@gmail.com>
2026-01-01 00:12:42 +0700

    removed deepcopy

--- jsonparser.py
+++ jsonparser.py
@@ -1,16 +1,16 @@
 import json
-import copy
 
-data_processed = [{"name": "Лунтик", "age": 1, "friends": [{"name": "Вупсень", "age": 23}, {"name": "Пупсень", "age": 23}]}]
+data_processed = [{"name": "Лунтик", "age": 1, "friends": [{"name": "Вупсень", "age": 3}, {"name": "Пупсень", "age": 23}]}]
 
 with open("data.json", 'w', encoding='utf-8') as file:
     json.dump(data_processed, file, ensure_ascii=False)
 
 def remove_friends_older_ten(elems_to_filter):
-    new_elems = copy.deepcopy(elems_to_filter)
-    for elem in new_elems:
-        new_friends = [friend for friend in elem['friends'] if friend['age'] <= 10]
-        elem['friends'] = new_friends
+    new_elems = []
+    for elem in elems_to_filter:
+        new_elem = elem.copy()
+        new_elem['friends'] = [friend for friend in elem['friends'] if friend['age'] <= 10]
+        new_elems.append(new_elem)
     return new_elems
 
 
