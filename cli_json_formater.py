2faad32179d1cc14b09734da1037c8165ebf0a80
daniilkuz <d.d.kuzovlev@gmail.com>
2026-01-02 12:32:42 +0700

    formated

--- cli_json_formater.py
+++ cli_json_formater.py
@@ -8,26 +8,34 @@ parser.add_argument("-fi", "--field")
 parser.add_argument("-v", "--value")
 parser.add_argument("-o", "--output")
 
+
 def open_json(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, "r", encoding="utf-8") as file:
         file_to_return = json.load(file)
-    
+
     return file_to_return
 
+
 def save_json(output, json_to_save):
-    with open(output, 'w', encoding='utf-8') as file:
+    with open(output, "w", encoding="utf-8") as file:
         json.dump(json_to_save, file, ensure_ascii=False)
 
+
 def add_field_to_json(filename, field, value):
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, "r", encoding="utf-8") as file:
         json_to_modify = json.load(file)
         for elem in json_to_modify:
             elem[field] = value
-    
+
     return json_to_modify
 
-if __name__=="__main__":
+
+def main():
     args = parser.parse_args()
     if args.action == "add":
         json_to_save = add_field_to_json(args.filename, args.field, args.value)
         save_json(args.output, json_to_save)
+
+
+if __name__ == "__main__":
+    main()
--- jsonparser.py
+++ jsonparser.py
@@ -1,22 +1,31 @@
 import json
 
-data_processed = [{"name": "Лунтик", "age": 1, "friends": [{"name": "Вупсень", "age": 3}, {"name": "Пупсень", "age": 23}]}]
+data_processed = [
+    {
+        "name": "Лунтик",
+        "age": 1,
+        "friends": [{"name": "Вупсень", "age": 3}, {"name": "Пупсень", "age": 23}],
+    }
+]
 
-with open("data.json", 'w', encoding='utf-8') as file:
+with open("data.json", "w", encoding="utf-8") as file:
     json.dump(data_processed, file, ensure_ascii=False)
+
 
 def remove_friends_older_ten(elems_to_filter):
     new_elems = []
     for elem in elems_to_filter:
         new_elem = elem.copy()
-        new_elem['friends'] = [friend for friend in elem['friends'] if friend['age'] <= 10]
+        new_elem["friends"] = [
+            friend for friend in elem["friends"] if friend["age"] <= 10
+        ]
         new_elems.append(new_elem)
     return new_elems
 
 
-with open('data.json', 'r', encoding='utf-8') as input_file:
+with open("data.json", "r", encoding="utf-8") as input_file:
     data = json.load(input_file)
     filtered_data = remove_friends_older_ten(data)
-    
-    with open('data_filtered.json', 'w', encoding='utf-8') as output_file:
+
+    with open("data_filtered.json", "w", encoding="utf-8") as output_file:
         json.dump(filtered_data, output_file, ensure_ascii=False)
